711d01060e6b4ca09dbbe9af4ce8dc5aeb26d055
Prabaningrum <prabaningrumhb@gmail.com>
2022-10-17 13:11:03 +0700

    update

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from PIL import Image
 model = tf.keras.models.load_model('model_imp.h5')
 
 st.title("Mask Detection")
+st.text("Welcome to the mask detection page. In here, you can drop any image of a person to detect whether a person is wearing a mask or not.")
 
-img = st.file_uploader("Upload an image")
+img = st.file_uploader("Upload an image of a person...", type="jpg")
 
 if st.button('Submit'):
 
     img2 = Image.open(img)
+    st.image(img2)
     img3 = np.array(img2)
     img_resize = cv2.resize(img3,(150, 150))    
     img_reshape = img_resize/255
